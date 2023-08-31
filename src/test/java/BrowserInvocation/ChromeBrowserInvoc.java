7d93cdcc3ff47b69ed5f5904a03707d99e4e04a5
dhanashree254 <dhanashreejadhav254@gmail.com>
2023-09-01 01:55:02 +0530

    add1

---
+++ src/test/java/BrowserInvocation/ChromeBrowserInvoc.java
@@ -0,0 +1,16 @@
+package BrowserInvocation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserInvoc {
+	
+	public static void main(String[] args) {
+		
+//		WebDriver driver = new ChromeDriver();
+	System.out.println(System.getProperty("user.dir"));	
+		
+		
+	}
+
+}
